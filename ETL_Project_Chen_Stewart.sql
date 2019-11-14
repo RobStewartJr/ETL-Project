fc82d072e632dcede7d9110ec4403e50d7f4ded1
robstewartjr[Aj <robastewartjr@gmail.com>
2019-11-13 19:38:55 -0600

    Final Push

--- ETL_Project_Chen_Stewart.sql
+++ ETL_Project_Chen_Stewart.sql
@@ -12,23 +12,7 @@ CREATE TABLE table_two (
  beta FLOAT
 );
 
-SELECT * FROM sector_two
-
-
-DROP TABLE sector
-DROP TABLE sectors
-DROP TABLE sector_two
-DROP TABLE sp
-DROP TABLE sp500
-DROP TABLE sp_two
-
-SELECT * FROM sp500
-
-SELECT * FROM table_one
-SELECT * FROM table_two
-
-
-
+--Joining table_one and table_two
 select *
 from table_one t1
 	inner join table_two t2 on t1.symbol = t2.symbol
